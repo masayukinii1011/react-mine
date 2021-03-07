97ade7b2df5f709f0ab007071cd31a24de728b4e
masayukinii1011 <masayukinii1011@gmail.com>
2021-03-07 19:12:47 +0900

    first commit

---
+++ src/components/game.js
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+import Board from './board';
+import '../index.css';
+
+export default function Game() {
+  const [history, setHistory] = useState(
+    [{
+      squares: Array(9).fill(null)
+    }]
+  );
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXIsNext] = useState(true);
+  //const [winner, setWinner] = useState(null);
+
+  function calculateWinner(squares){
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  function handleClick(i) {
+    if (calculateWinner(history[stepNumber].squares) || history[stepNumber].squares[i]) {
+      return;
+    }
+    history[stepNumber].squares[i] = xIsNext ? 'X' : 'O';
+    setHistory(history.concat([{
+      squares: history[stepNumber].squares
+    }]))
+    setStepNumber(history.length)
+    setXIsNext(!xIsNext)
+    console.log(history)
+  }
+
+  function jumpTo(step) {
+    setStepNumber(step)
+    setXIsNext((step % 2) === 0)
+    console.log(history)
+  }
+
+  const winner = calculateWinner(history[stepNumber].squares);
+
+  const moves = history.map((step, move) => {
+    const desc = move ? 'Go to move #' + move : 'Go to game start';
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  })
+
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={history[stepNumber].squares} onClick={(i) => handleClick(i)} />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/move.js
@@ -0,0 +1,84 @@
+import { useState } from 'react';
+import Board from './board';
+import '../index.css';
+
+export default function Game() {
+  const [history, setHistory] = useState(
+    [{
+      squares: Array(9).fill(null)
+    }]
+  );
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXIsNext] = useState(true);
+  const current = history[stepNumber];
+
+  function calculateWinner(squares){
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  function handleClick(i) {
+    const current = history[history.length - 1];
+    const squares = current.squares;
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = xIsNext ? 'X' : 'O';
+    setHistory(history.concat([{
+      squares: squares
+    }]))
+    setStepNumber(history.length)
+    setXIsNext(!xIsNext)
+  }
+
+ const winner = calculateWinner(current.squares);
+
+/*
+  const jumpTo = (step) => {
+    setStepNumber(step)
+    setXIsNext((step % 2) === 0)
+  }
+
+  const moves = history.map((step, move) => {
+    const desc = move ? 'Go to move #' + move : 'Go to game start';
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+  })
+*/
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={current.squares} onClick={(i) => handleClick(i)} />
+      </div>
+      <div className="game-info">
+        {/*<div>{status}</div>*/}
+        {/*<ol>{moves}</ol>*/}
+      </div>
+        {/*
+        <li key={move}>
+          {/*<button onClick={() => jumpTo(move)}>{desc}</button>
+        </li>
+        */}
+    </div>
+  );
+}
